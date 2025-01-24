64ccc54db7ebdc668fea2296963015e9794c51c5
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2025-01-24 14:59:23 +0400

    Slider added

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,29 +1,44 @@
 // select options
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdowns = document.querySelectorAll('.dropdown');
-  
-    dropdowns.forEach(dropdown => {
-      const select = dropdown.querySelector('.select');
-      const menu = dropdown.querySelector('.menu');
-  
-      select.addEventListener('click', function() {
-        dropdown.classList.toggle('open');
-        
-      });
-  
-      menu.addEventListener('click', function(e) {
-        if (e.target.tagName === 'LI') {
-          const selectedText = e.target.textContent;
-          dropdown.querySelector('.selected').textContent = selectedText;
-          dropdown.classList.remove('open');
-        }
-      });
-  
-      document.addEventListener('click', function(e) {
-        if (!dropdown.contains(e.target)) {
-          dropdown.classList.remove('open');
-        }
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach((dropdown) => {
+    const select = dropdown.querySelector(".select");
+    const menu = dropdown.querySelector(".menu");
+
+    select.addEventListener("click", function () {
+      dropdown.classList.toggle("open");
+    });
+
+    menu.addEventListener("click", function (e) {
+      if (e.target.tagName === "LI") {
+        const selectedText = e.target.textContent;
+        dropdown.querySelector(".selected").textContent = selectedText;
+        dropdown.classList.remove("open");
+      }
+    });
+
+    document.addEventListener("click", function (e) {
+      if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove("open");
+      }
     });
   });
-  
+});
+
+// burger menu
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("toggleDownMenu")
+    .addEventListener("click", function () {
+      var downMenu = document.querySelector(".down-Menu");
+      downMenu.style.display =
+        downMenu.style.display === "block" ? "none" : "block";
+    });
+});
+
+
+
+// slider
+
+
